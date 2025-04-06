bdb43aa9d2403a60c482c500996c55ebbcc5e32e
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-04-06 20:01:10 +0530

    edit text animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
+// tailwind.config.js
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      keyframes: {
+        shine: {
+          '0%': { backgroundPosition: '100%' },
+          '100%': { backgroundPosition: '-100%' },
+        },
+      },
       animation: {
-        'spin-fast': 'spin 8s linear infinite', // Faster rotation for a decorative effect
-        'bounce': 'bounce 13s infinite',        // Bouncing effect for hover
+        'spin-fast': 'spin 8s linear infinite',    // Custom spin animation
+        'bounce': 'bounce 13s infinite',           // Custom bounce animation
+        'shine': 'shine 5s linear infinite',       // Shine effect using keyframes
       },
     },
   },
   plugins: [],
 };
-
