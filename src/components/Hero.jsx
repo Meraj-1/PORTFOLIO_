b7bb7be2bd8a9a55c15a4c4409cc5af298a4e247
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-06-30 17:38:21 +0530

    add resume section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Icon } from "../assests/assets";
 import { motion } from "framer-motion";
 
-
 const Hero = () => {
-
   return (
     <div className="flex justify-center flex-col md:flex-row h-auto   lg:pt-20">
       <div className="md:m-20 m-2">
         <span className="font-bold md:text-[32px] text-[20px] text-white">
-          HI I'm MERAJ <span className="text-pink-400 font-extrabold"> Software Developer </span> &<br />{" "}
+          HI I'm MERAJ{" "}
+          <span className="text-pink-400 font-extrabold">
+            {" "}
+            Software Developer{" "}
+          </span>{" "}
+          &<br />{" "}
           <span className="text-pink-400 font-extrabold">Web Developer</span>
         </span>
         <div></div>
@@ -19,14 +22,24 @@ const Hero = () => {
           <br />
           performance and user experience.
         </span>
+        <div className="flex gap-10">
         {/* <span></span> */}
         <button className="text-pink-400 mt-10 flex px-6 py-1 border">
-         <a href="#contact"> Contact</a>
+          <a href="#contact"> Contact</a>
         </button>
+
+        <a
+          href="/meraj2.pdf"
+          download
+          className="text-pink-400 mt-10 flex px-6 py-1 border"
+        >
+          Resume
+        </a>
+        </div>
       </div>
       <div className="m-2">
         <motion.img
-         whileHover={{ scale: 1.05 }}
+          whileHover={{ scale: 1.05 }}
           className=" md:h-[386px] h-[300px]  cursor-pointer"
           src={Icon.main_hero}
           alt="Profile"
