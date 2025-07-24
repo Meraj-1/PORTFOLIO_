302c1784074820346e9567c3c8128f44ef0b209f
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-07-24 15:10:10 +0530

    refactor: remove unnecessary cursor-target classes for cleaner code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <TargetCursor 
         spinDuration={2}
         hideDefaultCursor={true}/>
-          <span className="cursor-target font-bold md:text-[32px] text-[20px] text-white">
+         <span className="cursor-target font-bold md:text-[32px] text-[20px] text-white">
           HI I'm MERAJ{" "}
           <span className="text-pink-400 font-extrabold">
             {" "}
@@ -21,21 +21,21 @@ const Hero = () => {
         </span>
         <div></div>
         <br />
-        <span className="cursor-target text-white text-sm">
+        <span className="text-white text-sm">
           I turn complex problems into elegant, efficient code with a focus on{" "}
           <br />
           performance and user experience.
         </span>
         <div className="flex gap-10">
         {/* <span></span> */}
-        <button className="cursor-target text-pink-400 mt-10 flex px-6 py-1 border">
+        <button className="text-pink-400 mt-10 flex px-6 py-1 border">
           <a href="#contact"> Contact</a>
         </button>
 
         <a
           href="/meraj2.pdf"
           download
-          className="cursor-target text-pink-400 mt-10 flex px-6 py-1 border"
+          className="text-pink-400 mt-10 flex px-6 py-1 border"
         >
           Resume
         </a>
@@ -44,7 +44,7 @@ const Hero = () => {
       <div className="m-2">
         <motion.img
           whileHover={{ scale: 1.05 }}
-          className="cursor-target md:h-[386px] h-[300px]  cursor-pointer"
+          className=" md:h-[386px] h-[300px]  cursor-pointer"
           src={Icon.main_hero}
           alt="Profile"
         />
