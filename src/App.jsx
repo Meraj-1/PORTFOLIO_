7dbad35570ecf58ec7d2050ed7588a142323b83c
Meraj-1 <belalraza158@gmail.com>
2026-01-01 23:56:15 +0530

    remove footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import About from './components/About';
 // import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Technology from './components/Technology';
 import InterviewFAQ from './components/InterViewFAQ';
@@ -35,7 +35,7 @@ const App = () => {
       {/* <Projects />    */}
       {/* <SkillPlayGround/> */}
       <Contact />
-    <Footer /> 
+    {/* <Footer />  */}
     </div>
     </div>
   </div>
